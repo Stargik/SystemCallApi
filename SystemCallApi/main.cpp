55f20c6d8c7cf9f63f04dfde000c956c601eda00
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-04-11 19:06:40 +0300

    Initial Commit

---
+++ SystemCallApi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SystemCallApi
+//
+//  Created by Aleksandr Starzhynskyi on 11.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
